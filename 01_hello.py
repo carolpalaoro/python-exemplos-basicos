143f6b3ce242e8b3c9385fde20bbd06677371cf6
carolpalaoro <carolpalaoro.senai@gmail.com>
2024-08-01 11:38:47 -0300

    Início
    inicio

---
+++ 01_hello.py
@@ -0,0 +1,2 @@
+# comentar
+print("hello world")
---
+++ 02_variaveis.py
@@ -0,0 +1,17 @@
+# Variáveis
+nome = "Gisele"
+idade = 44
+
+# Exibição de valores
+print(nome)
+print(idade)
+
+# 1 método de concatenação (string: str)
+print("O meu nome é: " + nome + "e tenho" + str(idade) + "anos.")
+
+# 2º método de concatenação (format)
+print("O meu nome é: {} e tenho {}  anos.\n ".format(nome, idade))
+
+# 3º método de concatenação (f string)
+print(f"O meu nome é: {nome} e tenho {idade} anos.")
+print("\n")
